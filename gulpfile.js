615c85fe7f617c74d40bde9c70c9881f7dc69b5e
Taehyun Park <root@njovy.com>
2022-12-09 12:14:40 +0900

    Simplify gulp and build process to avoid build errors on OSX

--- gulpfile.js
+++ gulpfile.js
@@ -12,17 +12,17 @@
  * limitations under the License.
  */
 
-const cleanCSS = require('gulp-clean-css');
+// const cleanCSS = require('gulp-clean-css');
 const closureBuilder = require('closure-builder');
 const closureCompiler = require('gulp-closure-compiler');
-const concatCSS = require('gulp-concat-css');
-const cssInlineImages = require('gulp-css-inline-images');
+// const concatCSS = require('gulp-concat-css');
+// const cssInlineImages = require('gulp-css-inline-images');
 const connect = require('gulp-connect');
 const fse = require('fs-extra');
-const flip = require('gulp-css-flip');
+// const flip = require('gulp-css-flip');
 const gulp = require('gulp');
 const path = require('path');
-const sass = require('gulp-sass')(require('sass'));
+// const sass = require('gulp-sass')(require('sass'));
 const streamqueue = require('streamqueue');
 const util = require('gulp-util');
 
@@ -370,16 +370,16 @@ function buildCss(isRtl) {
 }
 
 // Concatenates and minifies the CSS sources for LTR languages.
-gulp.task('build-css', () => buildCss(false));
+// gulp.task('build-css', () => buildCss(false));
 
 // Concatenates and minifies the CSS sources for RTL languages.
-gulp.task('build-css-rtl', () => buildCss(true));
+// gulp.task('build-css-rtl', () => buildCss(true));
 
 // Creates a webserver that serves all files from the root of the package.
 gulp.task('serve', () => {
-  connect.server({
-    port: 4000
-  });
+  // connect.server({
+  //   port: 4000
+  // });
 });
 
 // Deletes intermediate files.
@@ -388,13 +388,13 @@ gulp.task('clean', () => fse.remove(TMP_DIR));
 // Executes the basic tasks for the default language.
 gulp.task('default', gulp.series(
     'build-externs', 'build-ts', 'build-js',
-    'build-npm', 'build-esm', 'build-css', 'build-css-rtl',
+    'build-npm', 'build-esm',
     'clean'
 ));
 
 // Builds everything (JS for all languages, both LTR and RTL CSS).
 gulp.task('build-all', gulp.series(
     'build-externs', 'build-ts', 'build-all-js',
-    'build-npm', 'build-esm', 'build-css', 'build-css-rtl',
+    'build-npm', 'build-esm',
     'clean'
 ));
